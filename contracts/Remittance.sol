dd9b308f55a6047e21a19982e3df36e6f1c62e2e
joshorig <cassidy.joshua@gmail.com>
2017-08-22 13:19:24 +0100

    Code clean up round one

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,7 +4,7 @@ import "./Terminable.sol";
 
 contract Remittance is Terminable {
 
-  struct ExchangeRequest {
+  struct RemittanceRequestStruct {
     bool is_value;
     uint amount;
     uint fee;
@@ -12,7 +12,7 @@ contract Remittance is Terminable {
     uint deadline;
   }
 
-  struct ExchangeVenue {
+  struct RemittanceVenueStruct {
     bool is_value;
     bool is_active;
     uint fee;
@@ -20,96 +20,77 @@ contract Remittance is Terminable {
 
   uint private counter;
 
-  event LogExchangeRequest(address indexed sender, address indexed exchange, uint amount, uint fee, uint deadline);
+  event LogRemittanceRequest(address indexed sender, address indexed exchange, uint amount, uint fee, uint deadline);
   event LogWithdrawal(address indexed exchange, uint256 value);
   event LogRefund(address indexed sender, uint256 value);
 
-  mapping (address => ExchangeVenue) exchange_venues;
-  mapping (address => mapping (bytes32 => ExchangeRequest)) exchange_requests;
+  mapping (address => RemittanceVenueStruct) public remittanceVenues;
+  mapping (address => mapping (bytes32 => RemittanceRequestStruct)) remittanceRequests;
   address[] exchange_venue_addresses;
 
 
   uint public required_gas = 41580; //update for request method
 
-  function requestExchange(address _exchange_venue, bytes32 _public_key, uint _duration) public payable returns(bytes32 _id)  {
+  function requestRemittance(address _exchange_venue, bytes32 _public_key, uint _duration)
+  public
+  payable
+  {
     uint fee = required_gas * tx.gasprice; //safe multiply
     require(msg.value > fee && msg.sender.balance >= msg.value);
-    require(exchange_venues[_exchange_venue].is_value && exchange_venues[_exchange_venue].is_active); //exchange must exist and be active
+    require(remittanceVenues[_exchange_venue].is_value && remittanceVenues[_exchange_venue].is_active); //exchange must exist and be active
     uint amount = msg.value-fee;
     uint deadline = block.number+_duration;
-    _id = generateId(_exchange_venue,msg.sender);
-    exchange_requests[_exchange_venue][_public_key].is_value = true;
-    exchange_requests[_exchange_venue][_public_key].amount = amount;
-    exchange_requests[_exchange_venue][_public_key].fee = fee;
-    exchange_requests[_exchange_venue][_public_key].public_key = _public_key;
-    exchange_requests[_exchange_venue][_public_key].deadline = deadline;
-    LogExchangeRequest(msg.sender,_exchange_venue,amount,fee,deadline);
+    remittanceRequests[_exchange_venue][_public_key].is_value = true;
+    remittanceRequests[_exchange_venue][_public_key].amount = amount;
+    remittanceRequests[_exchange_venue][_public_key].fee = fee;
+    remittanceRequests[_exchange_venue][_public_key].public_key = _public_key;
+    remittanceRequests[_exchange_venue][_public_key].deadline = deadline;
+    LogRemittanceRequest(msg.sender,_exchange_venue,amount,fee,deadline);
   }
 
-  function checkSha3(bytes32 _hash1, bytes32 _hash2) constant returns (bytes32)
+  function withdraw(bytes32 _public_key, bytes32 _hash1, bytes32 _hash2)
+  public
   {
-    return sha3(_hash1,_hash2);
-  }
-
-  function withdraw(bytes32 _public_key, bytes32 _hash1, bytes32 _hash2) public returns (bool success) {
-    require(exchange_venues[msg.sender].is_value);
-    require(exchange_requests[msg.sender][_public_key].is_value);
-    require(sha3(_hash1,_hash2) == exchange_requests[msg.sender][_public_key].public_key);
-    uint withdrawal_amount = exchange_requests[msg.sender][_public_key].amount;
-    exchange_requests[msg.sender][_public_key].is_value = false;
-    msg.sender.transfer(withdrawal_amount);
+    require(remittanceVenues[msg.sender].is_value);
+    require(remittanceRequests[msg.sender][_public_key].is_value);
+    require(sha3(_hash1,_hash2) == remittanceRequests[msg.sender][_public_key].public_key);
+    uint withdrawal_amount = remittanceRequests[msg.sender][_public_key].amount;
+    remittanceRequests[msg.sender][_public_key].is_value = false;
     LogWithdrawal(msg.sender,withdrawal_amount);
-    return true;
+    msg.sender.transfer(withdrawal_amount);
   }
 
-  function refund(bytes32 _public_key, address _exchange_venue) public returns (bool success) {
-    require(exchange_venues[_exchange_venue].is_value);
-    require(exchange_requests[_exchange_venue][_public_key].is_value);
-    require(exchange_requests[_exchange_venue][_public_key].deadline<block.number);
-    exchange_requests[_exchange_venue][_public_key].is_value = false;
-    uint refund_amount = safeAdd(exchange_requests[_exchange_venue][_public_key].amount,exchange_requests[_exchange_venue][_public_key].fee);
-    msg.sender.transfer(refund_amount);
+  function refund(bytes32 _public_key, address _exchange_venue)
+  public
+  {
+    require(remittanceVenues[_exchange_venue].is_value);
+    require(remittanceRequests[_exchange_venue][_public_key].is_value);
+    require(remittanceRequests[_exchange_venue][_public_key].deadline<block.number);
+    remittanceRequests[_exchange_venue][_public_key].is_value = false;
+    uint refund_amount = safeAdd(remittanceRequests[_exchange_venue][_public_key].amount,remittanceRequests[_exchange_venue][_public_key].fee);
     LogRefund(msg.sender,refund_amount);
-    return true;
+    msg.sender.transfer(refund_amount);
   }
 
-  function registerExchangeVenue(uint _fee) public returns (bool success) {
-    require(!exchange_venues[msg.sender].is_value);
-    exchange_venues[msg.sender].is_value = true;
-    exchange_venues[msg.sender].is_active = true;
-    exchange_venues[msg.sender].fee = _fee;
-    exchange_venue_addresses.push(msg.sender);
-    return true;
+  function registerRemittanceVenue(uint _fee)
+  public
+  {
+    require(!remittanceVenues[msg.sender].is_value);
+    remittanceVenues[msg.sender].is_value = true;
+    remittanceVenues[msg.sender].is_active = true;
+    remittanceVenues[msg.sender].fee = _fee;
   }
 
-  function disableExchangeVenue() public returns (bool success) {
-    require(exchange_venues[msg.sender].is_value && exchange_venues[msg.sender].is_active);
-    exchange_venues[msg.sender].is_active = false;
-    return true;
+  function disableRemittanceVenue()
+  public
+  {
+    require(remittanceVenues[msg.sender].is_value && remittanceVenues[msg.sender].is_active);
+    remittanceVenues[msg.sender].is_active = false;
   }
 
-  function updateExchangeFee(uint _fee) public returns (bool success) {
-    require(exchange_venues[msg.sender].is_value);
-    exchange_venues[msg.sender].fee = _fee;
-    return true;
-  }
-
-  function activeExchanges() public constant returns (address[] exchanges, uint[] fees, bool[] active) {
-    address[] memory _exchanges = new address[](exchange_venue_addresses.length);
-    uint[] memory _fees = new uint[](exchange_venue_addresses.length);
-    bool[] memory _active = new bool[](exchange_venue_addresses.length);
-    for(uint i; i<exchange_venue_addresses.length; i++) {
-      if(exchange_venues[exchange_venue_addresses[i]].is_value && exchange_venues[exchange_venue_addresses[i]].is_active) {
-        _exchanges[i] = exchange_venue_addresses[i];
-        _fees[i] = exchange_venues[exchange_venue_addresses[i]].fee;
-        _active[i] = exchange_venues[exchange_venue_addresses[i]].is_active;
-      }
-    }
-    return (_exchanges,_fees,_active);
-  }
-
-  function generateId(address _exchange_venue, address _sender) internal returns (bytes32) {
-    return keccak256(_exchange_venue,_sender,counter++);
+  function updateRemittanceFee(uint _fee) public {
+    require(remittanceVenues[msg.sender].is_value);
+    remittanceVenues[msg.sender].fee = _fee;
   }
 
   function safeAdd(uint a, uint b) internal constant returns (uint c) {
--- test/remittance.js
+++ test/remittance.js
@@ -13,7 +13,7 @@ function before(Remittance,accounts,done)
   Remittance.deployed().then(function (instance) { //deploy it
   remittance = instance;
 
-  Promise.all(exchange_accounts.map((exchnage) => remittance.registerExchangeVenue(exchnage.fee, { from: exchnage.account })))
+  Promise.all(exchange_accounts.map((exchnage) => remittance.registerRemittanceVenue(exchnage.fee, { from: exchnage.account })))
     .then(() => done());
   });
 }
@@ -40,26 +40,6 @@ function solSha3 (...args) {
     return web3.sha3(args, { encoding: 'hex' })
 }
 
-contract('Remittance', function(accounts) {
-
-    beforeEach(function (done) {
-      before(Remittance,accounts,done);
-    });
-
-    it("Should register exchnages and return exchanges correctly", done => {
-        remittance.activeExchanges()
-        .then((results) => {
-          assert.equal(exchange_accounts[0].account,results[0][0],"Did not register first exchange correctly");
-          assert.equal(exchange_accounts[0].fee.toString(10),results[1][0].toString(10),"Did not register first exchange correctly");
-          assert.isOk(results[2][0],"Did not register first exchange correctly");
-
-          assert.equal(exchange_accounts[1].account,results[0][1],"Did not register second exchange correctly");
-          assert.equal(exchange_accounts[1].fee.toString(10),results[1][1].toString(10),"Did not register second exchange correctly");
-          assert.isOk(results[2][1],"Did not register second exchange correctly");
-          done();
-        })
-    });
-});
 
 contract('Remittance', function(accounts) {
 
@@ -73,7 +53,7 @@ contract('Remittance', function(accounts) {
     var passwordHash2 = solSha3(password2);
     var public_key = solSha3(passwordHash1,passwordHash2);
 
-    remittance.requestExchange(exchange_accounts[0].account, public_key, duration, {from: accounts[0],value: web3.toWei(amount_to_send,"ether")})
+    remittance.requestRemittance(exchange_accounts[0].account, public_key, duration, {from: accounts[0],value: web3.toWei(amount_to_send,"ether")})
     .then((txObject) => {
       //assert.isString(id.,"Did not create exchange request with an id");
       done();
@@ -96,7 +76,7 @@ contract('Remittance', function(accounts) {
     var starting_balance;
     var ending_balance;
 
-    remittance.requestExchange(exchange_accounts[0].account, public_key, duration, {from: accounts[0],value: web3.toWei(amount_to_send,"ether")})
+    remittance.requestRemittance(exchange_accounts[0].account, public_key, duration, {from: accounts[0],value: web3.toWei(amount_to_send,"ether")})
     .then((txObject) => {
       starting_balance = web3.eth.getBalance(exchange_accounts[0].account);
       remittance.withdraw(public_key, passwordHash1, passwordHash2, {from: exchange_accounts[0].account})
@@ -117,18 +97,13 @@ contract('Remittance', function(accounts) {
   beforeEach(function (done) {
     before(Remittance,accounts,done);
   });
-
+  var updated_fee = 300;
   it("Should update exchnage fee correctly", done => {
-    var updated_fee = 300;
-    remittance.updateExchangeFee(updated_fee, {from: exchange_accounts[0].account})
-    .then((success) => {
-      assert.isOk(success);
-      remittance.activeExchanges()
-      .then((results) => {
-        assert.equal(results[1][0].toString(10),updated_fee.toString(10),"Did not update first exchange correctly");
-        assert.equal(results[1][1].toString(10),exchange_accounts[1].fee.toString(10),"Did not leave second exchange unmodified");
-        done();
-      });
+    remittance.updateRemittanceFee(updated_fee, {from: exchange_accounts[0].account})
+    .then((txObject) => {
+
+      done();
+
     });
   });
 });
@@ -140,15 +115,10 @@ contract('Remittance', function(accounts) {
   });
 
   it("Should disable exchange", done => {
-    remittance.disableExchangeVenue({from: exchange_accounts[0].account})
-    .then((success) => {
-      assert.isOk(success);
-      remittance.activeExchanges()
-      .then((results) => {
-        assert.isNotOk(results[2][0],"Did not disable exchange");
-        assert.isOk(results[2][1],"Did not leave second exchange unmodified");
+    remittance.disableRemittanceVenue({from: exchange_accounts[0].account})
+    .then((txObject) => {
+
         done();
       });
-    });
   });
 });
